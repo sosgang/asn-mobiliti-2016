e35899fb92c956b67da95b8555d875cb9f162dae
fpoggi <francesco.poggi5@unibo.it>
2020-03-21 10:23:18 +0100

    Minor changes

--- script/createDbCurriculum/04.getAbstractsScopus.py
+++ script/createDbCurriculum/04.getAbstractsScopus.py
@@ -25,6 +25,55 @@ fileAuthorDoisMapping = "../../data/input/04.authorDoisMapping.json"
 fileListaDoisToDownload = "../../data/input/04.doisToDownload-list.txt"
 pathAbstracts = "../../data/output/abstracts/"
 
+
+import sqlite3
+from sqlite3 import Error
+
+import conf
+ 
+def create_connection(db_file):
+	""" create a database connection to the SQLite database
+		specified by db_file
+	:param db_file: database file
+	:return: Connection object or None
+	"""
+	conn = None
+	try:
+		conn = sqlite3.connect(db_file)
+		return conn
+	except Error as e:
+		print(e)
+ 
+	return conn
+ 
+ 
+def create_table(conn, create_table_sql):
+	""" create a table from the create_table_sql statement
+	:param conn: Connection object
+	:param create_table_sql: a CREATE TABLE statement
+	:return:
+	"""
+	try:
+		c = conn.cursor()
+		c.execute(create_table_sql)
+	except Error as e:
+		print(e)
+
+
+def create_eidDoi(conn, eidDoi):
+	"""
+	Create a new eidDoi mapping into the eidDoi table
+	:param conn:
+	:param dieDoi:
+	:return: eidDoi id
+	"""
+	sql = ''' INSERT INTO eidDoi(eid,doi)
+			  VALUES(?,?) '''
+	cur = conn.cursor()
+	cur.execute(sql, eidDoi)
+	return cur.lastrowid
+
+	
 def computeAndSave_authorDoisMapping_listaDoisToDownload(fileMapping,fileLista,numDoisForeachCandidate=5):
 	dois = set()
 	authorDois = dict()
@@ -103,22 +152,60 @@ authorDoisMapping = load_authorDoisMapping(fileAuthorDoisMapping)
 listaDoisToDownload = load__listaDoisToDownload(fileListaDoisToDownload)
 
 
-#filename_regex = os.path.join(pathAuthorsSearch, cognome.replace(" ","-") + '_' + nome.replace(" ","-") + '*') 
-contents = glob(pathAbstracts + "*.json")
-contents.sort()
-for filename_withPath in contents:
-	with open(filename_withPath) as json_file:
-		data = json.load(json_file)
-		try:
-			eid = data["abstracts-retrieval-response"]["coredata"]["eid"]
-		except:
-			print ("ERROR: NO EID - EXIT")
-			sys.exit()
-		try:
-			doi = data["abstracts-retrieval-response"]["coredata"]["prism:doi"]
-		except:
-			doi = None
-		print ("EID: %s - DOI: %s" % (eid,doi))
+
+
+def main():
+	
+	sql_create_eidDoi_table = """ CREATE TABLE IF NOT EXISTS eidDoi (
+										eid text NOT NULL,
+										doi text
+									  ); """
+	
+	# create a database connection
+	conn = create_connection(conf.dbFilename)
+ 
+	# create tables
+	if conn is not None:
+		
+		create_table(conn, sql_create_eidDoi_table)
+		
+		conn.close()
+	else:
+		print("Error! cannot create the database connection.")
+	
+	
+	
+	
+	eidDoiMap = dict()
+	doiEidMap = dict()
+	
+	conn = create_connection(conf.dbFilename)
+	with conn:
+		contents = glob(pathAbstracts + "*.json")
+		contents.sort()
+		for filename_withPath in contents:
+			with open(filename_withPath) as json_file:
+				data = json.load(json_file)
+				try:
+					eid = data["abstracts-retrieval-response"]["coredata"]["eid"]
+				except:
+					print ("ERROR: NO EID - EXIT")
+					sys.exit()
+				try:
+					doi = data["abstracts-retrieval-response"]["coredata"]["prism:doi"]
+				except:
+					doi = None
+				print ("EID: %s - DOI: %s" % (eid,doi))
+				
+				create_eidDoi(conn,(eid,Doi))
+				eidDoiMap[eid] = doi
+				if doi is not None:
+					doiEidMap[doi] = eid
+				
+if __name__ == '__main__':
+	main()
+
+
 
 '''
 for i in range(0,10):
