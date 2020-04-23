5383d2ba358b4f4724bb56ee3cacb351126f7b24
fpoggi <francesco.poggi5@unibo.it>
2020-04-23 10:55:00 +0200

    updated conf.py

--- script/createDbCurriculum/conf.py
+++ script/createDbCurriculum/conf.py
@@ -1,4 +1,5 @@
 dbFilename = "../../data/output/candidatesAsn2016.db"
+dbFilenameCercauniversita = "../../data/output/cercauniversitaUniBo.db"
 #dbFilename_sectors = "../data/output/statistici_settori.db"
 #sectors = ["13/D1","13/D2","13/D3"]
 anno = "2016"
